3af8a894a4aa07f274d72325750bd5698548cb63
Paul Makles <paulmakles@gmail.com>
2020-06-20 23:19:16 +0100

    Add temporary contact info

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,6 +9,10 @@ const Page: NextPage = () => (
 		<div className={global.container}>
 			I have migrated servers hence you're currently seeing the new beta website.<br/>
             More stuff to appear soon ™️<br/>
+            <br/>
+            <h3>Contact Me</h3>
+            Message me on Discord: <code>insert#0751</code><br/>
+            Or email me: <a href="mailto:me@insrt.uk">me@insrt.uk</a>.
 			<br/>
 			<a href="/post/website-design">See the website's design reference post.</a>
 		</div>
